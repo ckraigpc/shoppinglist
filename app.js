40821ff1834463eb96f2e4d38606ed90d6f7602e
ckraig <ckraigpc@gmail.com>
2018-05-15 13:29:55 -0700

    updated app.js

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const form = document.getElementById('shop-form');
 const inputItem = document.getElementById('item');
 const btnAddItem = document.querySelector('.btn');
 
+
+// declare variable inside 
+const cardCollection = document.querySelector('.card-action');
+
 main();
 
 function main (){
     form.addEventListener('submit',addItem);
+
+    cardCollection.addEventListener('click',deleteitem);
+
+    
+}
+function deleteitem(){
+    console.log('delete item');
 }
 function addItem(event){
     if(inputItem.value === ""){
@@ -19,16 +30,30 @@ function addItem(event){
         li.className = "collection-item";
         
         // insert the input value in the li
-        li.innerHTML = inputItem.value;
+        li.appendChild(document.createTextNode(inputItem.value));
         
         // define collection ul variable
         let ul = document.querySelector('.collection');
 
+    
+        // create a link on handling delete icond
+        let link = document.createElement('a');
+        
+        // add a classname on a link
+        link.className='delete-item secondary-content'
+
+        // append delete icon to link
+        link.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
+
+        // append link to list
+        li.appendChild(link);
+
         // append li to ul
         ul.appendChild(li)
-
         // console log elements
-        console.log(ul);
+        console.log(li);
+
+        // 
     }
     event.preventDefault();
 }
